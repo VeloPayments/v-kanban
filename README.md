a764f400416221e416db0b8a9c3e760ac1f29984
Justin Handville <nanolith@gmail.com>
2020-08-05 03:01:10 -0400

    Initial revision.

---
+++ README.md
@@ -0,0 +1,14 @@
+Project Kanban for Blockchain Demo
+==================================
+
+This kanban is maintained as a simple HTML file with some CSS markup.
+
+To run the board, you'll need Python 3.  Run the following command:
+
+```bash
+    python3 -m http.server
+```
+
+To update the board, look in kanban.js and follow the example provided.  Always
+append actions to the end of the `updateBoard` function so we can keep track of
+changes with the changelog.
---
+++ kanban.js
@@ -0,0 +1,194 @@
+var numstories = 0;
+
+function createStory(title, text) {
+    var story = document.createElement("div");
+    var heading = document.createElement("h3");
+    var storyno = ++numstories;
+    heading.innerHTML = "BD-" + storyno + ": " + title;
+
+    var para = document.createElement("p");
+    para.innerHTML = text;
+
+    story.setAttribute("class", "story");
+    story.appendChild(heading);
+    story.appendChild(para);
+
+    return story;
+}
+
+function createRelease(version, title) {
+    var release = document.createElement("div");
+    var heading = document.createElement("h3");
+    heading.innerHTML = "Release " + version + ": " + title;
+
+    release.setAttribute("class", "release");
+    release.appendChild(heading);
+
+    return release;
+}
+
+function promote(item, prev, next) {
+    prev.removeChild(item);
+    next.appendChild(item);
+}
+
+function updateBoard() {
+    var backlog = document.getElementById("backlog");
+    var selected = document.getElementById("selected");
+    var progress = document.getElementById("progress");
+    var done = document.getElementById("done");
+
+    var createKanban =
+        createStory(
+            "Create Kanban", "Create a kanban for the blockchain demo");
+    backlog.appendChild(createKanban);
+
+    var pubfileReader =
+        createStory(
+            "Add Public Entity Reader to agentd",
+            "The Public Entity Reader provides a safe way to import public " +
+            "entities into agentd.");
+    backlog.appendChild(pubfileReader);
+
+    var privateKeyReader =
+        createStory(
+            "Add Private Key Reader to agentd",
+            "The Private Key Reader provides a safe way to import the " +
+            "private key into agentd.");
+    backlog.appendChild(privateKeyReader);
+
+    var improvedProtocolKeyImport =
+        createStory(
+            "Improved key import into protocol service",
+            "This change improves the way we import keys into the protocol " +
+            "service.");
+    backlog.appendChild(improvedProtocolKeyImport);
+
+    var canonizationKeyControlAPI =
+        createStory(
+            "Extend the canonization service control API with key import",
+            "This change is the first step toward enabling signed blocks.");
+    backlog.appendChild(canonizationKeyControlAPI);
+
+    var improvedCanonizationKeyImport =
+        createStory(
+            "Improved key import into canonization service",
+            "This change pushes keys to the canonization service from the " +
+            "supervisor.");
+    backlog.appendChild(improvedCanonizationKeyImport);
+
+    var canonizationBlockSigning =
+        createStory(
+            "Enable block signing in the canonization service",
+            "This change enables block signing and block chaining in the " +
+            "canonization service.");
+    backlog.appendChild(canonizationBlockSigning);
+
+    var vcblockchainProtocol =
+        createStory(
+            "Add protocol support to vcblockchain",
+            "Allow both Java and C clients to use a shared protocol API.");
+    backlog.appendChild(vcblockchainProtocol);
+
+    var forwardSecrecy =
+        createStory(
+            "Enable forward secrecy in the protocol handshake",
+            "This provides minimum security guarantees in the case of key " +
+            "recovery.");
+    backlog.appendChild(forwardSecrecy);
+
+    var notificationServiceShell =
+        createStory(
+            "Notification Service Shell",
+            "Create the shell for the notification service.");
+    backlog.appendChild(notificationServiceShell);
+
+    var blockNotificationRegNotificationService =
+        createStory(
+            "API to register for a block notification",
+            "This is the notification-service specific implementation.");
+    backlog.appendChild(blockNotificationRegNotificationService);
+
+    var blockNotificationRegProtocolService =
+        createStory(
+            "Protocol Service provides block notification API",
+            "The protocol service provides an API method to register for a " +
+            "block-level notification.\n\n" +
+            "The client can assert that a given block ID is the latest; when " +
+            "this assertion fails, the client will receive an " +
+            "invalidation.\n\n" +
+            "The protocol service handles cleanup when the connection closes.");
+    backlog.appendChild(blockNotificationRegProtocolService);
+
+    var blockNotificationDataService =
+        createStory(
+            "Data Service sends a new block to the notification service",
+            "This enables the block invalidation logic from canonization to " +
+            "client updates, enabling client sentinels on block update.");
+    backlog.appendChild(blockNotificationDataService);
+
+    var mesonJavaLibBuild =
+        createStory(
+            "Meson for the Java Lib build",
+            "Ensure that we can build the Java library using OSS components.");
+    backlog.appendChild(mesonJavaLibBuild);
+
+    var demo1Versioning =
+        createStory(
+            "Version tagging for Demo 1",
+            "Ensure that there are official source versions for this " +
+            "tutorial.");
+    backlog.appendChild(demo1Versioning);
+
+    var article1Overview =
+        createStory(
+            "Article 1: Blockchain Overview",
+            "Overview of the OSS Blockchain Project.");
+    backlog.appendChild(article1Overview);
+
+    var article2Demo1 =
+        createStory(
+            "Article 1: Tutorial 1",
+            "Tutorial for the first demo.");
+    backlog.appendChild(article2Demo1);
+
+    /** Project actions **/
+
+    //Administrative tasks for setup.
+    promote(createKanban, backlog, selected);
+    promote(createKanban, selected, progress);
+    promote(createKanban, progress, done);
+
+    //Create Demo 1 release.
+    var releaseDemo1Selected = createRelease("0.4.0", "Demo 1");
+    selected.appendChild(releaseDemo1Selected);
+    var releaseDemo1Progress = createRelease("0.4.0", "Demo 1");
+    progress.appendChild(releaseDemo1Progress);
+    var releaseDemo1Done = createRelease("0.4.0", "Demo 1");
+    done.appendChild(releaseDemo1Done);
+
+    //Stories for Demo 1
+    promote(pubfileReader, backlog, releaseDemo1Selected);
+    promote(privateKeyReader, backlog, releaseDemo1Selected);
+    promote(improvedProtocolKeyImport, backlog, releaseDemo1Selected);
+    promote(canonizationKeyControlAPI, backlog, releaseDemo1Selected);
+    promote(improvedCanonizationKeyImport, backlog, releaseDemo1Selected);
+    promote(canonizationBlockSigning, backlog, releaseDemo1Selected);
+    promote(mesonJavaLibBuild, backlog, releaseDemo1Selected);
+    promote(demo1Versioning, backlog, releaseDemo1Selected);
+    promote(article1Overview, backlog, releaseDemo1Selected);
+    promote(article2Demo1, backlog, releaseDemo1Selected);
+
+    //Create Demo 2 release.
+    var releaseDemo2Backlog = createRelease("0.4.1", "Demo 2");
+    backlog.appendChild(releaseDemo2Backlog);
+
+    //Stories for Demo 2
+    promote(vcblockchainProtocol, backlog, releaseDemo2Backlog);
+    promote(forwardSecrecy, backlog, releaseDemo2Backlog);
+    promote(notificationServiceShell, backlog, releaseDemo2Backlog);
+    promote(
+        blockNotificationRegNotificationService, backlog, releaseDemo2Backlog);
+    promote(blockNotificationRegProtocolService, backlog, releaseDemo2Backlog);
+    promote(blockNotificationDataService, backlog, releaseDemo2Backlog);
+}
